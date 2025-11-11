5ed17ead966cd6332538938b8a033feae0726ec4
abdurrahman0810 <abdurrahman0810@student.abudzar.sch.id>
2025-11-11 09:15:32 +0700

    change height

--- styl-prlx.js
+++ styl-prlx.js
@@ -5,8 +5,10 @@ window.addEventListener("scroll", () => {
   let value = window.scrollY;
   console.log(value);
 
-  Left.style.left = value * 3.7 + "px";
-  Left.style.transform = `scale(${1 + value * 0.0037} )`;
-  Right.style.transform = `scale(${1 + value * 0.0037} )`;
-  Right.style.right = value * 3.7 + "px";
+  if (value <= 530) {
+    Left.style.left = value * 3.7 + "px";
+    Left.style.transform = `scale(${1 + value * 0.0037} )`;
+    Right.style.transform = `scale(${1 + value * 0.0037} )`;
+    Right.style.right = value * 3.7 + "px";
+  }
 });
