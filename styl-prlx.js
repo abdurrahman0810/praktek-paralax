ab029a6a19908f92a6e6421e5f2fab6eb62feeb1
abdurrahman0810 <abdurrahman0810@student.abudzar.sch.id>
2025-11-12 11:47:02 +0700

    section-2

--- styl-prlx.js
+++ styl-prlx.js
@@ -1,10 +1,12 @@
 let Left = document.getElementById("Left");
 let Right = document.getElementById("Right");
+let imgBack = document.getElementById("img-back-2");
 
 window.addEventListener("scroll", () => {
   let value = window.scrollY;
   console.log(value);
 
+  imgBack.style.transform = `rotate(${0.8 * value}deg )`;
   if (value <= 530) {
     Left.style.left = value * 3.9 + "px";
     Left.style.transform = `scale(${1 + value * 0.0037} )`;
